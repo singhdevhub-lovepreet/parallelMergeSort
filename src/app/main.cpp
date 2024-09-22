574aa541afedd2bff77c7259bedb7d668a5e0ef0
lovepreet-singh <lovepreet.singh@gmail.com>
2024-09-23 01:44:02 +0530

    Working Parallel Merge sort with huge diff

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <mutex>
+#include <chrono> // Include chrono for timing
 #include "sorting/mergeSort.hpp"
 #include "sorting/parallelMergeSort.hpp"
-#include <vector>
 
 int main(int argc, char *argv[]) {
-    std::vector<int> nums = {5, 2, 1, 10, 8, 7};
-    MergeSort* mergesort = new MergeSort(&nums);
-    mergesort->sort();
 
-    std::cout << "Sorted array is: " << std::endl;
-    for (int i = 0; i < nums.size(); i++) {
-        std::cout << nums[i] << std::endl;
+    const int SIZE = 10000000;
+    std::vector<int> nums(SIZE);
+    std::vector<int> nums1(SIZE);
+    for (int i = 0; i < SIZE; ++i) {
+        nums[i] = rand() % 10000000;
+        nums1[i] = nums[i];
     }
+
+    
+    MergeSort* mergesort = new MergeSort(&nums);
+    auto start = std::chrono::high_resolution_clock::now();
+    mergesort->sort();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> mergeSortDuration = end - start;
+
+    std::cout << "MergeSort time taken: " << mergeSortDuration.count() << " seconds" << std::endl;
+
     delete mergesort;
 
-    std::vector<int> nums1 = {5, 2, 1, 10, 8, 7};
+    // Measure time for ParallelMergeSort
     ParallelMergeSort* mergesort1 = new ParallelMergeSort(&nums1);
+    start = std::chrono::high_resolution_clock::now();
     mergesort1->sort();
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> parallelMergeSortDuration = end - start;
 
-    std::cout << "Sorted array is: " << std::endl;
-    for (int i = 0; i < nums1.size(); i++) {
-        std::cout << nums1[i] << std::endl;
-    }
+    std::cout << "ParallelMergeSort time taken: " << parallelMergeSortDuration.count() << " seconds" << std::endl;
+
     delete mergesort1;
 
     return 0;
--- src/app/sorting/parallelMergeSort.cpp
+++ src/app/sorting/parallelMergeSort.cpp
@@ -8,7 +8,7 @@ ParallelMergeSort::~ParallelMergeSort() {}
 
 void ParallelMergeSort::recursiveSort(int left, int right) {
 
-    const int THRESHOLD = 100;
+    const int THRESHOLD = 5000;
 
     if (right - left < THRESHOLD) {
         std::sort(nums->begin() + left, nums->begin() + right + 1);
